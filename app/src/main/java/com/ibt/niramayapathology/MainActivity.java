33625963b693e76e232e9520652a2136376f5564
Pushpendra Bhati <pushpendra.infobite@gmail.com>
2019-03-29 16:50:22 +0530

    pathology module

---
+++ app/src/main/java/com/ibt/niramayapathology/MainActivity.java
@@ -0,0 +1,13 @@
+package com.ibt.niramayapathology;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
